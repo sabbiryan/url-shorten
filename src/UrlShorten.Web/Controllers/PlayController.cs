389c2a2a658365be7660d5c65d326b9b2d7c2685
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2019-09-25 05:28:02 +0600

    test api replaced by play

---
+++ src/UrlShorten.Web/Controllers/PlayController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlShorten.Service.TinyUrls.Dto;
+
+namespace UrlShorten.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayController: ControllerBase
+    {
+
+        public PlayController()
+        {
+            
+        }
+
+        // GET: api/Play/s
+        [HttpGet("{relativePath}", Name = "PlayGet")]
+        public async Task Get([FromHeader]string relativePath)
+        {
+            await Task.Delay(0);
+        }
+    }
+}
