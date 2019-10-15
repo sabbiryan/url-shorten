02a9f2ef69d195e4f0d75c58b490ca5050894973
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2019-10-15 23:20:24 +0600

    cors enabled

--- src/UrlShorten.Web/Startup.cs
+++ src/UrlShorten.Web/Startup.cs
@@ -34,6 +34,18 @@ namespace UrlShorten.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
